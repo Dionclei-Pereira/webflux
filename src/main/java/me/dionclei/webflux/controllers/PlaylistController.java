26a6bdde15bd63b4d986185e1aff54bccc3ccbc8
Dionclei de Souza Pereira <122059269+Dionclei-Pereira@users.noreply.github.com>
2025-02-07 22:33:00 -0300

    Save playlist

--- src/main/java/me/dionclei/webflux/controllers/PlaylistController.java
+++ src/main/java/me/dionclei/webflux/controllers/PlaylistController.java
@@ -1,9 +1,13 @@
 package me.dionclei.webflux.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +32,15 @@ public class PlaylistController {
 	@GetMapping("/{id}")
 	public Mono<ResponseEntity<Playlist>> findById(@PathVariable String id) {
 		return playlistService.findById(id).map(ResponseEntity::ok);
+	}
+	
+	@PostMapping
+	public Mono<ResponseEntity<Playlist>> save(@RequestBody Playlist playlist, ServerHttpRequest request) {
+	    return playlistService.save(playlist) 
+	        .map(savedPlaylist -> ResponseEntity
+	            .created(URI.create(request.getURI().toString() + "/" + savedPlaylist.getId()))
+	            .body(savedPlaylist)
+	        );
 	}
 	
 }
